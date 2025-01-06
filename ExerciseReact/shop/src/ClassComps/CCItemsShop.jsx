cd21b2b2d261aa45975adb96c73b228da823dc98
NathanMJ <nathanmimoun2001@gmail.com>
2025-01-06 19:31:58 +0200

    change

--- ExerciseReact/shop/src/ClassComps/CCItemsShop.jsx
+++ ExerciseReact/shop/src/ClassComps/CCItemsShop.jsx
@@ -8,13 +8,13 @@ constructor(props) {
     
  setImg = () => {
         return `../src/Pictures/${this.props.name}.jpg`
-    }
+    } 
 
 
 
   render() {
     return (
-        <div style={{width:'100%',display:'flex',flexDirection:'column',border:'rgb(175, 66, 166) solid 5px',borderRadius:'20px',backgroundColor:'rgb(225, 170, 230)',padding:'10px',gap:'20px'}}>
+        <div style={{width:'100%',display:'flex',flexDirection:'column',border:'rgb(57, 148, 190) solid 5px',borderRadius:'20px',backgroundColor:'rgb(170, 238, 243)',padding:'10px',gap:'20px'}}>
             <h2>{this.props.name}</h2>
             <img style={{width:250}} src={this.setImg()}></img>
             <h2>{this.props.price} ₪</h2>
