7bf184da22ea23cee46cf26376ca5c75ceafd6dc
NathanMJ <nathanmimoun2001@gmail.com>
2025-01-09 14:19:48 +0200

    chg

--- TestReactLunette/test-glasses/src/App.jsx
+++ TestReactLunette/test-glasses/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      Hello<br />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
     </>
   )
 }
---
+++ TestReactLunette/test-glasses/src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+        <img src="" alt="head" />        
+        <img src="" alt="hat" />        
+        <img src="" alt="glasses" />
+        <img src="" alt="earing" />
+    </>
+  )
+}
