8cb40f87c8f67d812c843d499cfe1d0d109c200f
NathanMJ <nathanmimoun2001@gmail.com>
2025-04-23 15:04:53 +0300

    f

---
+++ StackPrj/StackPrj/Node.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Node<T>
+    {
+        private T value;
+        private Node<T> next;
+
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+
+        public Node(T value, Node<T> next)
+        {
+            this.value = value;
+            this.next = next;
+        }
+
+        public T GetValue()
+        {
+            return value;
+        }
+
+        public Node<T> GetNext()
+
+        {
+            return next;
+        }
+
+        public bool HasNext()
+        {
+            return next != null;
+        }
+
+        public void SetValue(T value)
+        {
+            this.value = value;
+        }
+
+        public void SetNext(Node<T> next)
+        {
+            this.next = next;
+        }
+
+        public override string ToString()
+        {
+            if (next == null)
+            {
+                return value + " --> null";
+            }
+
+            return value + " --> " + next;
+        }
+    }
+}
