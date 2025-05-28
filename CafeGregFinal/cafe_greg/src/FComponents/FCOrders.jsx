7af0692144917517e60d235fffc89c331169b5af
NathanMJ <nathanmimoun2001@gmail.com>
2025-05-28 09:43:24 +0300

    .

--- CafeGregFinal/cafe_greg/src/FComponents/FCOrders.jsx
+++ CafeGregFinal/cafe_greg/src/FComponents/FCOrders.jsx
@@ -1,23 +1,23 @@
 export default function FCOrders(props) {
     return (
         <div className="orders">
-            {props.orders.map((order) => {
+            {props.orders.map((order, index) => {
                 return (
-                    <div className='order'>
+                    <div className='order' key={index}>
                         <div className="mainOrder">
                             <img src={order.img} />
                             <h1>{order.name}</h1>
                             <h2>{order.price}</h2>
                         </div>
                         <div className="details">
-                            {order.ingredients.map((ingredient) => {
+                            {order.ingredients.map((ingredient, index) => {
                                 return (
-                                    <p>{ingredient}</p>
+                                    <p key={index}>{ingredient}</p>
                                 )
                             })}
-                            {order.changes.map((change) => {
+                            {order.changes.map((change, index) => {
                                 return (
-                                    <p>{change.change} {change.ingredient}</p>
+                                    <p key={index}>{change.change} {change.ingredient}</p>
                                 )
                             })}
                         </div>
