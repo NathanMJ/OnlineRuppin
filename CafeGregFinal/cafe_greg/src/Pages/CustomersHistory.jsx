3fbdb8f3c795c3dc3e7d3d5ec11752f6ba96af2b
NathanMJ <nathanmimoun2001@gmail.com>
2025-06-12 12:14:25 +0300

    .

--- CafeGregFinal/cafe_greg/src/Pages/CustomersHistory.jsx
+++ CafeGregFinal/cafe_greg/src/Pages/CustomersHistory.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom"
 
 export default function CustomersHistory(props) {
@@ -6,26 +7,50 @@ export default function CustomersHistory(props) {
 
     const { customers } = location.state
 
-    console.log(location.state);
-    
+    //Customers filter is the id of the customer for filter
+    const [customersFilter, setCustomersFilter] = useState([])
+
+    useEffect(() => {
+        console.log(customersFilter);
+    }, [customersFilter])
+
     /*TODO :
         SHOW HISTORY 
         CAN FILTER ACCORDING TO CUSTOMERS
             CAN SELECT MULTIPLY CUSTOMERS
 
 */
+
+    const checkACustomer = (id) => {
+        console.log(id);
+
+        const exist = customersFilter.some(eachId => eachId == id)
+
+        if (exist) {
+            let newCustomerFilter = customersFilter.filter(eachId => eachId != id)
+            setCustomersFilter(newCustomerFilter)
+        }
+        else {
+            let newCustomerFilter = [...customersFilter, id]
+            setCustomersFilter(newCustomerFilter)
+        }
+
+    }
+
+
     return (
         <div className="customerHistory">
             <div className="filterSide">
                 <div className="customers">
                     {customers.map((customer, index) => (
-                        <div className="customer" key={index}>
+                        <div className="customer" onClick={() => checkACustomer(customer.id)} key={index}>
+                            <div className="check"></div>
                             <h1>{customer.name}</h1>
                         </div>
                     ))}
                 </div>
             </div>
-            <div className="orderSide">
+            <div className="historySide">
 
             </div>
         </div>
