a7b8800fa318d0e2047386c189b4766fb17f4a19
NathanMJ <nathanmimoun2001@gmail.com>
2025-06-20 19:23:46 +0300

    slide pannel finished

--- CafeGregAppLink/app-link/app/(registerTab)/_layout.jsx
+++ CafeGregAppLink/app-link/app/(registerTab)/_layout.jsx
@@ -1,22 +1,159 @@
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { Tabs } from "expo-router";
-import { Ionicons } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
+import { Text, TouchableOpacity, View } from "react-native";
+
+
+import { useState } from "react";
+import { ScrollView, StyleSheet } from 'react-native';
+
+import { useEffect, useRef } from 'react';
+import { Animated, Dimensions } from 'react-native';
+import { hide } from 'expo-splash-screen';
+
+
+
 
 export default function TabLayout() {
+
+
+
+    const screenWidth = Dimensions.get('window').width;
+    const panelWidth = screenWidth * 0.8;
+
+    const slideAnim = useRef(new Animated.Value(-panelWidth)).current;
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        // This code runs only the first time the component mounts
+        // Place your one-time logic here
+        console.log("First time arriving at login screen");
+    }, []);
+
+    const togglePanel = () => {
+        Animated.timing(slideAnim, {
+            toValue: isVisible ? -panelWidth : 0,
+            duration: 300,
+            useNativeDriver: false, // ← ici !
+        }).start(() => setIsVisible(!isVisible));
+    };
+
+
+    const [customers, setCustomers] = useState([
+        { name: 'Nathan', id: '345538268' },
+        { name: 'Nathan', id: '345538268' },
+        { name: 'Nathan', id: '345538268' },
+        { name: 'Nathan', id: '345538268' },
+        { name: 'Nathan', id: '345538268' },
+        { name: 'Nathan', id: '345538268' },
+        { name: 'Nathan', id: '345538268' },
+        { name: 'Nathan', id: '345538268' },
+        { name: 'Nathan', id: '345538268' },
+        { name: 'Nathan', id: '345538268' },
+        { name: 'John', id: '345532268' }
+    ])
+
+
+
     return (
-        <Tabs screenOptions={{ headerShown: false }}>
-            <Tabs.Screen name="login" options={{
-                title: "Login",
-                tabBarIcon: ({ color, size }) => (
-                    <Ionicons name="person" size={size} color={color} />
-                )
-            }}></Tabs.Screen>
-            <Tabs.Screen name="register" options={{
-                title: "Register",
-                tabBarIcon: ({ color, size }) => (
-                    <MaterialIcons name="person" size={size} color={color} />
-                ),
-            }}></Tabs.Screen>
-        </Tabs>
+        <View style={{ flex: 1 }}>
+            <View style={{
+                position: 'absolute',
+                flexDirection: 'row',
+                height: '90%',
+                width: '100%',
+                zIndex: 1,
+                pointerEvents: 'box-none'
+            }}>
+
+                <Animated.View style={[styles.panel, { transform: [{ translateX: slideAnim }] }]}>
+                    <ScrollView style={{ height: '100%', overflow: 'scroll' }}>
+                        <Text style={{ textAlign: 'center', fontSize: 33 }}>Logged customers :</Text>
+                        {customers?.length > 0 ? customers.map((customer, index) => (
+                            <View key={index}>
+                                <Text style={{ textAlign: 'center', fontSize: 40 }}>{customer.name}</Text>
+                                <Text style={{ textAlign: 'center', fontSize: 20 }}>(Tz : {customer.id})</Text>
+                            </View>
+                        )) :
+                            <Text>No logged one</Text>}
+                    </ScrollView>
+
+                    <TouchableOpacity
+                        style={styles.toggleButtonFloating}
+                        onPress={togglePanel}
+                    >
+                        <Ionicons name="person-circle-outline" size={50} color="black" />
+                    </TouchableOpacity>
+                </Animated.View>
+            </View>
+            <Tabs screenOptions={{ headerShown: false }}>
+                <Tabs.Screen name="login" options={{
+                    title: "Login",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="person" size={size} color={color} />
+                    )
+                }} />
+                <Tabs.Screen name="register" options={{
+                    title: "Register",
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialIcons name="person" size={size} color={color} />
+                    )
+                }} />
+            </Tabs>
+        </View >
+
     )
 }
+
+const styles = StyleSheet.create({
+
+    backgroundImage: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center'
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#ddd',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    toggleButton: {
+        backgroundColor: '#333',
+        padding: 10,
+        borderRadius: 5,
+        marginBottom: 20,
+    },
+    toggleText: {
+        color: '#fff',
+        fontSize: 18,
+    },
+    panel: {
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        height: '100%',
+        width: '80%',
+        backgroundColor: '#fff',
+        padding: 20,
+        elevation: 5,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: { width: 2, height: 2 },
+        borderBottomRightRadius: 30,
+
+    },
+    toggleButtonFloating: {
+        position: 'absolute',
+        right: -70,
+        top: '10%',
+        transform: [{ translateY: -25 }],
+        backgroundColor: 'white',
+        borderRadius: 30,
+        padding: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 10,
+    }
+
+});
--- CafeGregAppLink/app-link/app/(registerTab)/login.jsx
+++ CafeGregAppLink/app-link/app/(registerTab)/login.jsx
@@ -2,12 +2,38 @@ import { Ionicons } from '@expo/vector-icons';
 import { useState } from "react";
 import { ImageBackground, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
+import React, { useRef } from 'react';
+import { Animated, Dimensions } from 'react-native';
+import { useEffect } from "react";
+
+
 export default function login() {
 
 
+    const screenWidth = Dimensions.get('window').width;
+    const panelWidth = screenWidth * 0.8;
 
-    
+    const slideAnim = useRef(new Animated.Value(-panelWidth)).current;
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        // This code runs only the first time the component mounts
+        // Place your one-time logic here
+        console.log("First time arriving at login screen");
+    }, []);
+
+    const togglePanel = () => {
+        Animated.timing(slideAnim, {
+            toValue: isVisible ? -panelWidth : 0,
+            duration: 300,
+            useNativeDriver: false, // ← ici !
+        }).start(() => setIsVisible(!isVisible));
+    };
+
+
     const [customers, setCustomers] = useState([
+        { name: 'Nathan', id: '345538268' },
+        { name: 'Nathan', id: '345538268' },
         { name: 'Nathan', id: '345538268' },
         { name: 'Nathan', id: '345538268' },
         { name: 'Nathan', id: '345538268' },
@@ -19,7 +45,6 @@ export default function login() {
         { name: 'John', id: '345532268' }
     ])
 
-    const [pannelIsOpen, setPannelIsOpen] = useState(true)
 
     return (
         <ImageBackground source={{
@@ -27,39 +52,35 @@ export default function login() {
         }}
             style={styles.backgroundImage}
             blurRadius={3}>
-            <View className='container' style={{
-                backgroundColor: 'white', position: 'absolute', height: '80%', width: `${pannelIsOpen ? '80%' : '0%'}`, borderTopRightRadius: 30, borderBottomRightRadius: 30,
-                alignItems: 'center', left: 0,
-            }}>
-                <View style={{
-                    position: 'absolute',
-                    left: '100%',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    marginLeft: 5
-                }}>
 
-                    <TouchableOpacity style={{
-                        backgroundColor: 'white',
-                        borderRadius: 30,
-                        padding: 5,
-                        alignItems: 'center',
-                        justifyContent: 'center'
-                    }} onPress={() => setPannelIsOpen(!pannelIsOpen)}>
+            {/* <View style={{
+                position: 'absolute',
+                flexDirection: 'row',
+                height: '100%', 
+                width: '100%', 
+            }}>
+
+                <Animated.View style={[styles.panel, { transform: [{ translateX: slideAnim }] }]}>
+                    <ScrollView style={{ height: '100%', overflow: 'scroll' }}>
+                        <Text style={{ textAlign: 'center', fontSize: 33 }}>Logged customers :</Text>
+                        {customers?.length > 0 ? customers.map((customer, index) => (
+                            <View key={index}>
+                                <Text style={{ textAlign: 'center', fontSize: 40 }}>{customer.name}</Text>
+                                <Text style={{ textAlign: 'center', fontSize: 20 }}>(Tz : {customer.id})</Text>
+                            </View>
+                        )) :
+                            <Text>No logged one</Text>}
+                    </ScrollView>
+
+                    <TouchableOpacity
+                        style={styles.toggleButtonFloating}
+                        onPress={togglePanel}
+                    >
                         <Ionicons name="person-circle-outline" size={50} color="black" />
                     </TouchableOpacity>
-                </View>
-                <ScrollView style={{ height: '100%', overflow: 'scroll' }}>
-                    <Text style={{ textAlign: 'center', fontSize: 33 }}>Logged customers :</Text>
-                    {customers?.length > 0 ? customers.map((customer, index) => (
-                        <View key={index}>
-                            <Text style={{ textAlign: 'center', fontSize: 40 }}>{customer.name}</Text>
-                            <Text style={{ textAlign: 'center', fontSize: 20 }}>(Tz : {customer.id})</Text>
-                        </View>
-                    )) :
-                        <Text>No logged one</Text>}
-                </ScrollView>
-            </View>
+                </Animated.View>
+            </View> */}
+
         </ImageBackground>
     )
 }
@@ -71,5 +92,47 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
         justifyContent: 'center'
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#ddd',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    toggleButton: {
+        backgroundColor: '#333',
+        padding: 10,
+        borderRadius: 5,
+        marginBottom: 20,
+    },
+    toggleText: {
+        color: '#fff',
+        fontSize: 18,
+    },
+    panel: {
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        height: '100%',
+        width: '80%',
+        backgroundColor: '#fff',
+        padding: 20,
+        elevation: 5,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: { width: 2, height: 2 },
+    },
+    toggleButtonFloating: {
+        position: 'absolute',
+        right: -70,
+        top: '10%',
+        transform: [{ translateY: -25 }],
+        backgroundColor: 'white',
+        borderRadius: 30,
+        padding: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 10,
     }
-})
+
+});
