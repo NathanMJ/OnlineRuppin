25aa9923a5aad8c6eb71bcac9aadf30cb82a10c3
NathanMJ <nathanmimoun2001@gmail.com>
2025-10-19 13:25:56 +0300

    .

--- CafeGregFinal/cafe_greg/src/App.jsx
+++ CafeGregFinal/cafe_greg/src/App.jsx
@@ -13,32 +13,47 @@ import KitchenBarPreparation from './Pages/KitchenBarPreparation.jsx'
 
 function App() {
 
-      const navigate = useNavigate()
+  const navigate = useNavigate()
 
-      const goto = (path, states) => {
-        navigate(path, { relative: true, state: states });
-      }
+  const goto = (path, states) => {
+    navigate(path, { relative: true, state: states });
+  }
+
+  const [profilName, setProfilName] = useState(null)
+
+  const login = (login, password) => {
+    const profilFound = 'CafeGreg'
+    if (profilFound) {
+      console.log('connect with login');
+      setProfilName(profilFound)
+
+    }
+    else {
+      console.log('dont connect with login');
+    }
+
+  }
 
   return (
     <>
       <div>
         <Routes>
-          <Route path='/' element={<ErrorPage goto={goto}/>}></Route>
-          <Route path='/login' element={<LoginWebsite goto={goto}/>}></Route>
-          <Route path='/sideChoice' element={<SideChoice goto={goto}/>}></Route>
-          <Route path='/workMain' element={<WorkMain goto={goto}/>}></Route>
-          <Route path='/toggleService' element={<ToggleService goto={goto}/>}></Route>
-          <Route path='/cafeMain' element={<CafeMain goto={goto}/>}></Route>
-          <Route path='/menu' element={<Menu goto={goto}/>}></Route>
-          <Route path='/productPage' element={<ProductPage goto={goto}/>}></Route>
-          <Route path='/customerRegisterLogin' element={<CustomerRegisterLogin goto={goto}/>}></Route>
-          <Route path='/customerHistory' element={<CustomersHistory goto={goto}/>}></Route>
-          <Route path='/kitchenBarPreparation' element={<KitchenBarPreparation goto={goto}/>}></Route>          
-          <Route path='/manager' element={<ManagerPage goto={goto}/>}></Route>          
-          <Route path='/manager/workers' element={<ManagerWorkerPage goto={goto}/>}></Route>          
-          <Route path='/manager/products' element={<ManagerProductPage goto={goto}/>}></Route>          
-          <Route path='/manager/stats' element={<ManagerStats goto={goto}/>}></Route>          
-          <Route path='/manager/sendMessageToTables' element={<ManagerSendMessageToTables goto={goto}/>}></Route>          
+          <Route path='/' element={<ErrorPage goto={goto} />}></Route>
+          <Route path='/login' element={<LoginWebsite goto={goto} login={login}/>}></Route>
+          <Route path='/sideChoice' element={<SideChoice goto={goto} />}></Route>
+          <Route path='/workMain' element={<WorkMain goto={goto} />}></Route>
+          <Route path='/toggleService' element={<ToggleService goto={goto} />}></Route>
+          <Route path='/cafeMain' element={<CafeMain goto={goto} />}></Route>
+          <Route path='/menu' element={<Menu goto={goto} />}></Route>
+          <Route path='/productPage' element={<ProductPage goto={goto} />}></Route>
+          <Route path='/customerRegisterLogin' element={<CustomerRegisterLogin goto={goto} />}></Route>
+          <Route path='/customerHistory' element={<CustomersHistory goto={goto} />}></Route>
+          <Route path='/kitchenBarPreparation' element={<KitchenBarPreparation goto={goto} />}></Route>
+          <Route path='/manager' element={<ManagerPage goto={goto} />}></Route>
+          <Route path='/manager/workers' element={<ManagerWorkerPage goto={goto} />}></Route>
+          <Route path='/manager/products' element={<ManagerProductPage goto={goto} />}></Route>
+          <Route path='/manager/stats' element={<ManagerStats goto={goto} />}></Route>
+          <Route path='/manager/sendMessageToTables' element={<ManagerSendMessageToTables goto={goto} />}></Route>
         </Routes>
       </div>
     </>
@@ -55,4 +70,5 @@ import ManagerWorkerPage from './Pages/ManagerWorkerPage.jsx'
 import ManagerProductPage from './Pages/ManagerProductPage.jsx'
 import ManagerStats from './Pages/ManagerStats.jsx'
 import ManagerSendMessageToTables from './Pages/ManagerSendMessageToTables.jsx'
+import { useState } from 'react'
 export const socket = io("http://localhost:5500");
